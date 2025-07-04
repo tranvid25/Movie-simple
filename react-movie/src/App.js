b4c420e649d50c05be735ec8f25ef4b4378413eb
tranvid25 <lsdnh041004@gmail.com>
2025-07-04 18:45:54 +0700

    Menu&Banner

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -1,8 +1,52 @@
-import logo from "./logo.svg";
-
+import { Fragment } from "react/jsx-runtime";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faYoutube } from "@fortawesome/free-brands-svg-icons";
 
 function App() {
-  return <div>abc</div>;
+  return (
+    <Fragment>
+      <header className="flex items-start justify-center py-10 mb-10 text-white header gap-x-5">
+        <span className="text-primary">Home</span>
+        <span>Movie</span>
+      </header>
+
+      <section className="banner h-[500px] page-container border border-solid border-black">
+        <div className="relative w-full h-full ">
+          <div className="absolute inset-0 rounded-lg overlay bg-gradient-to-t "></div>
+          <img
+            src="https://static1.cbrimages.com/wordpress/wp-content/uploads/2022/04/the-avengers-feature.jpg"
+            alt=""
+            className="object-center w-full h-full rounded-sm"
+          />
+          <div className="absolute w-full bottom-5 left-5">
+            <h2 className="mb-3 text-3xl font-bold text-white">Avengers: EndGame</h2>
+            <div className="flex items-center pb-3 gap-x-3">
+              <span className="p-2 font-medium text-purple-300 border border-white rounded-lg">
+                Adventure
+              </span>
+              <span className="p-2 font-medium text-purple-300 border border-white rounded-lg">
+                Action
+              </span>
+              <span className="p-2 font-medium text-purple-300 border border-white rounded-lg">
+                Funny
+              </span>
+            </div>
+
+            <div className="w-[100px]">
+              <button className="flex px-6 py-3 font-semibold text-white transition bg-purple-400 rounded-lg hover:text-red-500 gap-x-3">
+                <h1 className="">Watch now</h1>
+                <FontAwesomeIcon
+                  icon={faYoutube}
+                  size="3x"
+                  className="transition cursor-pointer text-white-600"
+                />
+              </button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </Fragment>
+  );
 }
 
 export default App;

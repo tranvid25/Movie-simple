8473cd356a87c39589504d4d7a0a1ff356f57c61
tranvid25 <lsdnh041004@gmail.com>
2025-07-04 19:35:32 +0700

    movie-card

--- react-movie/src/App.js
+++ react-movie/src/App.js
@@ -9,8 +9,7 @@ function App() {
         <span className="text-primary">Home</span>
         <span>Movie</span>
       </header>
-
-      <section className="banner h-[500px] page-container border border-solid border-black">
+      <section className="banner h-[500px] page-container border border-solid border-black mb-20">
         <div className="relative w-full h-full ">
           <div className="absolute inset-0 rounded-lg overlay bg-gradient-to-t "></div>
           <img
@@ -19,7 +18,9 @@ function App() {
             className="object-center w-full h-full rounded-sm"
           />
           <div className="absolute w-full bottom-5 left-5">
-            <h2 className="mb-3 text-3xl font-bold text-white">Avengers: EndGame</h2>
+            <h2 className="mb-3 text-3xl font-bold text-white">
+              Avengers: EndGame
+            </h2>
             <div className="flex items-center pb-3 gap-x-3">
               <span className="p-2 font-medium text-purple-300 border border-white rounded-lg">
                 Adventure
@@ -42,6 +43,72 @@ function App() {
                 />
               </button>
             </div>
+          </div>
+        </div>
+      </section>
+      <section className="pb-20 movies-layout page-container">
+        <h2 className="mb-5 text-3xl font-bold text-white capitalize">
+          Now playing
+        </h2>
+        <div className="grid grid-cols-4 gap-10 movie-list">
+          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
+            <img
+              src="https://tse3.mm.bing.net/th/id/OIP.mzwYTqDkEmU6QKX3MMhPyAHaEK?pid=Api&P=0&h=180"
+              alt=""
+              className="w-full h-[250px] object-center rounded-lg mb-5"
+            />
+            <h3 className="mb-3 text-xl font-bold">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between mb-10 text-sm text-opacity-50">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full p-5 text-xl font-semibold bg-pink-400 rounded-lg">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="pb-20 movies-layout page-container">
+        <h2 className="mb-5 text-3xl font-bold text-white capitalize">
+          Top Rated
+        </h2>
+        <div className="grid grid-cols-4 gap-10 movie-list">
+          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
+            <img
+              src="https://tse3.mm.bing.net/th/id/OIP.mzwYTqDkEmU6QKX3MMhPyAHaEK?pid=Api&P=0&h=180"
+              alt=""
+              className="w-full h-[250px] object-center rounded-lg mb-5"
+            />
+            <h3 className="mb-3 text-xl font-bold">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between mb-10 text-sm text-opacity-50">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full p-5 text-xl font-semibold bg-pink-400 rounded-lg">
+              Watch Now
+            </button>
+          </div>
+        </div>
+      </section>
+      <section className="pb-20 movies-layout page-container">
+        <h2 className="mb-5 text-3xl font-bold text-white capitalize">
+          Trending
+        </h2>
+        <div className="grid grid-cols-4 gap-10 movie-list">
+          <div className="p-3 text-white rounded-lg movie-card bg-slate-800">
+            <img
+              src="https://tse3.mm.bing.net/th/id/OIP.mzwYTqDkEmU6QKX3MMhPyAHaEK?pid=Api&P=0&h=180"
+              alt=""
+              className="w-full h-[250px] object-center rounded-lg mb-5"
+            />
+            <h3 className="mb-3 text-xl font-bold">Spiderman: Homecomming</h3>
+            <div className="flex items-center justify-between mb-10 text-sm text-opacity-50">
+              <span>2017</span>
+              <span>7.4</span>
+            </div>
+            <button className="w-full p-5 text-xl font-semibold bg-pink-400 rounded-lg">
+              Watch Now
+            </button>
           </div>
         </div>
       </section>

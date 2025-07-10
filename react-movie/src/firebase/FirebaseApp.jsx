c1328314e5f49c0b83e35febeb8cd150fbf2d680
tranvid25 <lsdnh041004@gmail.com>
2025-07-10 10:34:57 +0700

    delete

--- react-movie/src/firebase/FirebaseApp.jsx
+++ react-movie/src/firebase/FirebaseApp.jsx
@@ -1,11 +1,12 @@
-import { collection, getDocs,addDoc } from "firebase/firestore";
+import { collection, getDocs, addDoc,deleteDoc, doc } from "firebase/firestore";
 import React, { useEffect, useState } from "react";
 import { db } from "./firebase-config";
 
 const FirebaseApp = () => {
   const colRef = collection(db, "posts");
-  const [title,setTitle]=useState('');
-  const [author,setAuthor]=useState('');
+  const [title, setTitle] = useState("");
+  const [author, setAuthor] = useState("");
+  const [postId,setPostId]=useState("");
   useEffect(() => {
     getDocs(colRef).then((snapshot) => {
       let posts = [];
@@ -15,19 +16,26 @@ const FirebaseApp = () => {
           ...doc.data(),
         });
       });
-  
     });
   }, []);
-  const handleAddPost=(e)=>{
+  const handleAddPost = (e) => {
     e.preventDefault();
-    addDoc(colRef,{
-        title,
-        author
-    }).then(()=>{
-        console.log('success');
-    }).catch((error)=>{
-        console.log(error);
+    addDoc(colRef, {
+      title,
+      author,
     })
+      .then(() => {
+        console.log("success");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+  const handleRemovePost=async(e)=>{
+  e.preventDefault();
+  const colRefDelete=doc(db,'posts',postId);
+  await deleteDoc(colRefDelete);
+   
   }
   return (
     <div className="p-10">
@@ -35,13 +43,42 @@ const FirebaseApp = () => {
         <form onSubmit={handleAddPost}>
           <input
             type="text"
-            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500" placeholder="Enter your title" name="title" onChange={(e)=>setTitle(e.target.value)}
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your title"
+            name="title"
+            onChange={(e) => setTitle(e.target.value)}
           />
           <input
             type="text"
-            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500" placeholder="Enter your author" name="author" onChange={(e)=>setAuthor(e.target.value)}
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your author"
+            name="author"
+            onChange={(e) => setAuthor(e.target.value)}
           />
-        <button type="submit" className="w-full p-3 text-sm font-semibold text-white bg-blue-500 rounded-lg">Add post</button>
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-semibold text-white bg-blue-500 rounded-lg"
+          >
+            Add post
+          </button>
+        </form>
+      </div>
+      <div className="w-full max-w-[500px] mx-auto bg-white shadow-lg p-5">
+        <form onSubmit={handleRemovePost}>
+          <input
+            type="text"
+            className="w-full p-3 mb-5 border border-gray-200 rounded outline-none focus:border-blue-500"
+            placeholder="Enter your title"
+            name="postId"
+            onChange={(e) => setPostId(e.target.value)}
+          />
+
+          <button
+            type="submit"
+            className="w-full p-3 text-sm font-semibold text-white bg-blue-500 rounded-lg"
+          >
+            remove post
+          </button>
         </form>
       </div>
     </div>
